bdf5a5f5d2ddf8e6638daf1f6f79b98e2dba6a2f
Dr-ZHUIM <372919916@qq.com>
2022-11-22 13:34:28 +0800

    static class

--- lesson2/Dog.java
+++ lesson2/Dog.java
@@ -1,8 +1,23 @@
+/**
+ * Typical Class demonstration
+ * @instance_variable Can have as many of these as you want.
+ * @constructor Can instantiate a instance of the class
+ * @unstatic_method Can be called by a calss instance
+ * @static_method Can be called by the calss
+*/
 public class Dog {
-    public static void makeNoise() {
-        System.out.println("Bark!");
+    public int _weight;
+
+    public Dog(int weight) {
+        _weight = weight;
     }
-    public static void main(String[] args) {
-        makeNoise();
+    public void makeNoise() {
+        if (_weight <= 10) {
+            System.out.println("yip!");
+        } else if (_weight < 30) {
+            System.out.println("woooof");
+        } else {
+            System.out.println("Bark!");
+        }
     }
 }
--- lesson2/DogLuncher.java
+++ lesson2/DogLuncher.java
@@ -1,6 +1,13 @@
-/** The DogLuncher class tries to 'test drive' the Dog class. */
-public class DogLuncher{
+/**
+ * The DogLuncher class tries to 'test drive' the Dog class.
+ * @ 
+ * Declaration of a Dog variable -- Dog d;
+ * @
+ * Instantiation and Assignment -- d = new Dog(33);
+ */
+public class DogLuncher {
     public static void main(String[] args) {
-        Dog.makeNoise();
+        Dog d = new Dog(33);
+        d.makeNoise();
     }
 }
