7d78ec132174289a4637f1ecfe85f3f90991b9b4
Dr-ZHUIM <37291991@qq.com>
2022-11-24 22:15:39 +0800

    sentinel

--- lesson4/SLList.java
+++ lesson4/SLList.java
@@ -11,12 +11,28 @@ public class SLList {
     }
   }
   
-  private IntNode first;
+  /** The sentinel item (if it exists) is at sentinel.next
+   * @point1 rename first to be sentinel
+   * @point2 sentinel is never  null
+   * @point3 sentinel node's item needs to be some integer, but doesn't matter what value we pick
+   * @point4 Had to fix constructors and methods to be compatible with sentinel nodes
+   */
+  private IntNode sentinel;
   public int size;
+
+  /** Creates a empty SLList */
+
+  public SLList(){
+    sentinel = new IntNode(12,null);
+    size = 0;
+  }
+
   public SLList(int x){
-    first = new IntNode(x,null);
+    sentinel = new IntNode(12,null);
+    sentinel.next = new IntNode(x,null);
     size  = 1;
   }
+
   public static void main(String[] args) {
     SLList L = new SLList(15);
     L.addFirst(10);
@@ -27,18 +43,18 @@ public class SLList {
 
   /** Adds an item to the front of the list */
   public void addFirst(int x){
-    first = new IntNode(x,first);
+    sentinel.next = new IntNode(x,sentinel.next);
     size += 1;
   }
 
-  /** Returns the first item in the list */
+  /** Returns the sentinel item in the list */
   public int getFirst() {
-    return first.item;  
+    return sentinel.next.item;  
   }
 
   /** Adds an item to the end of the list */
   public void addLast(int x) {
-    IntNode p = first;
+    IntNode p = sentinel;
     /** Move last until ite reaches the end of the list */
     while (p.next != null) {
       p = p.next;
@@ -53,7 +69,7 @@ public class SLList {
   }
 
   public int size(){
-    return getSize(first);
+    return getSize(sentinel);
   }
 }
 
