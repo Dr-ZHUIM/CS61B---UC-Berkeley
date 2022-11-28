353e4b2003d8c787eb8eabdfd7f0ae16e0913475
Dr-ZHUIM <37291991@qq.com>
2022-11-28 22:44:05 +0800

    project 0

--- lesson5/ArrayBasic.java
+++ lesson5/ArrayBasic.java
@@ -1,20 +1,43 @@
-
 public class ArrayBasic {
-  public static void main(String[] args){
+
+  private static class TestClass {
+    int _a;
+    int _b;
+    public TestClass(int a, int b) {
+      _a = a;
+      _b = b;
+    }
+  }
+
+  public static void main(String[] args) {
+    String fieldOfInterest =  "_a";
+    TestClass test = new TestClass(2,3);
+    double testvalue = test._a;
+    System.out.println(testvalue);
     int[] z = null;
-    int[] x,y;
-    x = new int[]{1,2,3,4,5};
+    int[] x, y;
+    x = new int[] { 1, 2, 3, 4, 5 };
     y = x;
-    x = new int[]{-1,2,5,4,99};
+    x = new int[] { -1, 2, 5, 4, 99 };
     y = new int[3];
     z = new int[0];
     int xL = x.length;
-    
+
     String[] s = new String[6];
     s[4] = "ketchup";
     s[x[3] - x[1]] = "muffins";
 
-    int[] b = {9,10,11};
+    int[] b = { 9, 10, 11 };
     System.arraycopy(b, 0, x, 3, 2);
   }
 }
+
+/**
+ * Arrays vs Classes
+ * Arrays and Classes can both be used to organize a bunch of memory boxes.
+ * Arrays boxes are accessed using [] notation.
+ * Class boxes are accessed using dot notation.
+ * Array boxes must all be of the same type.
+ * Class boxes can be of different types.
+ * Both have a fixed number of boxes.
+ */
---
+++ skeletions/proj0/Planet.java
@@ -0,0 +1,30 @@
+public class Planet {
+    /**  Its current x position */
+    public double xxPos;
+    /**  Its current y position */
+    public double yyPos;
+    /**  ts current velocity in the x direction*/
+    public double xxVel;
+    /** Its current velocity in the y direction */
+    public double yyVel;
+    /**  Its mass */
+    public double mass;
+    /** The name of the file that corresponds to the image that depicts the planet (for example .junpier.gif) */
+    public String imgFileName;
+    public Planet(double xP, double yP, double xV, double yV, double m, String img){
+      xxPos = xP;
+      yyPos = yP;
+      xxVel = xV;
+      yyVel = yV;
+      mass = m;
+      imgFileName = img;
+    }
+    public Planet(Planet p){
+      xxPos = p.xxPos;
+      yyPos = p.yyPos;
+      xxVel = p.xxVel;
+      yyVel = p.yyVel;
+      mass = p.mass;
+      imgFileName = p.imgFileName;
+    }
+}
