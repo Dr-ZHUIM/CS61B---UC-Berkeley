3eedc1fcb524c71bf81637a1ac0ba05df7f9e61f
Dr-ZHUIM <37291991@qq.com>
2022-12-07 23:35:07 +0800

    lesson6 AList

---
+++ lesson6_ALists/AList.java
@@ -0,0 +1,57 @@
+package lesson6_ALists;
+
+/* Invariant
+ *  - addLast: The next item we want to add, will go into position size.
+ *  - getLast: The item we want to return is in position size - 1.
+ *  - size: The nubmer of items in the list should be size.
+ */
+
+public class AList<Item> {
+    private Item[] items;
+    private int size;
+
+    /** Create an empty list */
+    public AList() {
+        items = (Item[]) new Object[100];
+        size = 0;
+    }
+
+    private void resize(int capacity) {
+        Item[] newArray = (Item[]) new Object[capacity];
+        System.arraycopy(items, 0, newArray, 0, size);
+        items = newArray;
+    }
+
+    /** Inserts X into the back of the list: */
+    public void addLast(Item x) {
+        if (size == items.length) {
+            resize(size + 1);
+        }
+        items[size] = x;
+        size += 1;
+    }
+
+    /** Gets the last from the back of the list */
+    public Item getLast() {
+        return items[size - 1];
+    }
+
+    /** Gets the ith item in the list */
+    public Item get(int i) {
+        return items[i];
+    }
+
+    /** Gets the numbers of items in the list */
+    public int size() {
+        return size;
+    }
+
+    /** Deletes the last item from back of the list and return deleted Item */
+    public Item removeLast() {
+        Item returnItem = getLast();
+        items[size - 1] = null;
+        size -= 1;
+        return returnItem;
+    }
+
+}
---
+++ lesson6_ALists/SpeedTestAList.java
@@ -0,0 +1,13 @@
+package lesson6_ALists;
+
+public class SpeedTestAList {
+    public static void main(String[] args) {
+
+        AList L = new AList();
+        int i = 0;
+        while (i < 10000) {
+            L.addLast(i);
+            i++;
+        }
+    }
+}
