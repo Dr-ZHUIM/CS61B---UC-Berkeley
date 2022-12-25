6b97707bb734fc392172531537e90200735a08c9
Dr-ZHUIM <37291991@qq.com>
2022-12-25 20:08:35 +0800

    test selection sort in java

--- lesson7_Test/Sort.java
+++ lesson7_Test/Sort.java
@@ -4,13 +4,40 @@ public class Sort {
   /**
    * - find the smallest item
    * - move it to the first
-   * - selection sort the rest  
+   * - selection sort the rest
    */
-  public static void SelectionSort(String[] array){
+  public static void SelectionSort(String[] array) {
 
   }
 
-  public static String[] sort(String[] input) {
-    return input;
+  // step I find the smallest item
+  public static int findSmallestElementIndex(String[] input, int startIndex) {
+    int smallElementIndex = startIndex;
+    for (int i = startIndex; i < input.length; i++) {
+      if (input[i].compareTo(input[smallElementIndex]) < 0) {
+        smallElementIndex = i;
+      }
+    }
+    return smallElementIndex;
+  }
+
+  // step II move it to the first
+  public static void swap(String[] input, int index1, int index2) {
+    String cache = input[index1];
+    input[index1] = input[index2];
+    input[index2] = cache;
+  }
+
+  public static void sort(String[] input) {
+    sort(input, 0);
+  }
+
+  private static void sort(String[] input, int index) {
+    if (index == input.length) {
+      return;
+    }
+    int smallElementIndex = findSmallestElementIndex(input,index);
+    swap(input, index, smallElementIndex);
+    sort(input, index + 1);
   }
 }
--- lesson7_Test/TestSort.java
+++ lesson7_Test/TestSort.java
@@ -3,23 +3,38 @@ package lesson7_Test;
 public class TestSort {
   /** This will test the Sort.sort() method */
   public static void testSort() {
-    String[] input = new String[] { "You", "had", "breakfast", "didn't", "you" };
-    // String[] expected = new String[]{"You", "had", "breakfast", "didn't", "you"};
-    String[] expected = new String[] { "didn't", "you", "You", "had", "breakfast" };
-
+    String[] input = new String[] { "i", "have", "an", "egg" };
+    String[] expected = new String[] { "an", "egg", "have", "i"};
     Sort.sort(input);
-
     org.junit.Assert.assertArrayEquals(expected, input);
+  }
 
-    // for(int i = 0; i < expected.length; i++){
-    // if(!expected[i].equals(input[i])){
-    // System.out.println("Error! Mismatch in position " + i + ", expected: " +
-    // expected[i] + ", but found: " + input[i] );
-    // }}
+  /** test the Sort.findeSmallestElement method. */
+  public static void testFindSmallestElement() {
+    String[] input = { "i", "have", "an", "egg" };
+    int expected = 2;
+    String[] input2 = {"there","are","many","pigs"};
+    int expected2 = 1;
+    int actual = Sort.findSmallestElementIndex(input,0);
+    org.junit.Assert.assertEquals(expected, actual);
+    int actual2 = Sort.findSmallestElementIndex(input2,0);
+    org.junit.Assert.assertEquals(expected2, actual2);
+  }
 
+  public static void testSwap(){
+    String[] input = { "i", "have", "an", "egg" };
+    int a = 0;
+    int b = 2;
+    String expectedA = "an";
+    String expectedB = "i";
+    Sort.swap(input, a, b);
+    org.junit.Assert.assertEquals(expectedA, input[a]);
+    org.junit.Assert.assertEquals(expectedB, input[b]);
   }
 
   public static void main(String[] args) {
-    TestSort.testSort();
+    testSwap();
+    testSort();
+    testFindSmallestElement();
   }
 }
